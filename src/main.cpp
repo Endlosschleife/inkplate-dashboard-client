7d9701947aed90fc8c97b922a8350765a7b88024
Torben Lottmann <torben@torbstoff.de>
2022-09-08 13:59:23 +0200

    feat: use rtc for inkplate 10

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "config.h"
 #include "ArduinoJson.h"
 
+#define ENABLE_RTC // fime
+
 uint64_t MS_TO_S_FACTOR = 1000000;
 
 Inkplate display(INKPLATE_1BIT);
@@ -12,26 +14,35 @@ DynamicJsonDocument doc(ESP.getMaxAllocHeap());
 
 void sendToDeepSleep(int seconds)
 {
-    //Isolate/disable GPIO12 on ESP32 (only to reduce power consumption in sleep)
-    //rtc_gpio_isolate(GPIO_NUM_12);
-    // go to sleep
     Serial.println("Going to deep sleep for " + String(seconds) + " seconds now.");
-    uint64_t msToWakeup = seconds * MS_TO_S_FACTOR;
+
+#ifdef ENABLE_RTC
+    // rtc sleep
+    Serial.println("Use RTC for deep sleep");
+    Serial.println(display.rtcGetEpoch());
+    display.rtcSetAlarmEpoch(display.rtcGetEpoch() + seconds, RTC_ALARM_MATCH_DHHMMSS);
+    esp_sleep_enable_ext0_wakeup(GPIO_NUM_39, 0);
+    esp_deep_sleep_start();
+#else
+    // non rtc sleep
+    Serial.println("Use timer for deep sleep.");
+    uint64_t msToWakeup = 10 * MS_TO_S_FACTOR;
     Serial.print("Sleep in ms: ");
     Serial.println(msToWakeup);
     esp_sleep_enable_timer_wakeup(msToWakeup);
     esp_deep_sleep_start();
+#endif
 }
 
 void connectWifi()
 {
     Serial.println("Connect to wifi...");
 
-    //Connect to the WiFi network.
+    // Connect to the WiFi network.
     int attempts = 0;
     WiFi.mode(WIFI_MODE_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASS);
-    while (WiFi.status() != WL_CONNECTED && attempts < 5)
+    while (WiFi.status() != WL_CONNECTED && attempts < 20)
     {
         attempts++;
         delay(500);
@@ -61,16 +72,68 @@ void connectWifi()
     Serial.println("done.");
 }
 
+void drawImage()
+{
+    // Draw the second image from web, this time using a HTTPClient to fetch the response manually.
+    // Full color 24 bit images are large and take a long time to load, will take around 20 secs.
+    HTTPClient http;
+    // Set parameters to speed up the download process.
+    http.getStream().setNoDelay(true);
+    http.getStream().setTimeout(1);
+
+    // Photo taken by: Roberto Fernandez
+    http.begin("https://varipass.org/neowise.bmp");
+
+    // Check response code.
+    int httpCode = http.GET();
+    if (httpCode == 200)
+    {
+        // Get the response length and make sure it is not 0.
+        int32_t len = http.getSize();
+        if (len > 0)
+        {
+            if (!display.drawBitmapFromWeb(http.getStreamPtr(), 0, 0, len))
+            {
+                // If is something failed (wrong filename or wrong bitmap format), write error message on the screen.
+                // REMEMBER! You can only use Windows Bitmap file with color depth of 1, 4, 8 or 24 bits with no
+                // compression!
+                display.println("Image open error");
+                display.display();
+            }
+            display.display();
+        }
+        else
+        {
+            display.println("Invalid response length");
+            display.display();
+        }
+    }
+    else
+    {
+        display.println("HTTP error");
+        display.display();
+    }
+}
+
 void setup()
 {
-    Serial.begin(9600);
+    // Serial.begin(9600); fixme deep sleep does not work properly when this is activated...
     display.begin();
+
+#ifdef ENABLE_RTC
+    display.rtcClearAlarmFlag(); // Clear alarm flag from any previous alarm
+    if (!display.rtcIsSet())     // Check if RTC is already is set. If ts not, set time and date
+    {
+        display.rtcSetTime(0, 0, 0);
+        display.rtcSetDate(6, 16, 5, 2020);
+    }
+#endif
 
     connectWifi();
 
     // load config
     HTTPClient http;
-    http.begin(String(SERVER_URL) + "/dashboard/livingroom");
+    http.begin(String(SERVER_URL) + "/dashboard/calendar");
     int httpCode = http.GET();
     if (httpCode != 200)
     {
@@ -110,7 +173,8 @@ void setup()
     wakeup_reason = esp_sleep_get_wakeup_cause();
 
     // whether the screen should be fully updated (true) or partial update is enough (false)
-    boolean shouldFullyUpdatedScreen = wakeup_reason != ESP_SLEEP_WAKEUP_TIMER || (wakeupCounter % 6) == 0; // always if not woken up by timer
+    boolean shouldFullyUpdatedScreen = (wakeup_reason != ESP_SLEEP_WAKEUP_TIMER && wakeup_reason != ESP_SLEEP_WAKEUP_EXT0) 
+        || (wakeupCounter % 6) == 0; // always if not woken up by timer
 
     Serial.print("Should fully update: ");
     Serial.println(shouldFullyUpdatedScreen);
@@ -118,7 +182,7 @@ void setup()
     if (!shouldFullyUpdatedScreen)
     {
         Serial.println("rebuild previous screen");
-        if (!display.drawImage(archivedImageUrl, display.PNG, 0, 0, false, false))
+        if (!display.drawImage(archivedImageUrl, 0, 0, false, true))
         {
             shouldFullyUpdatedScreen = true;
         }
@@ -130,7 +194,7 @@ void setup()
     display.clearDisplay();
 
     Serial.println("load new screen");
-    if (!display.drawImage(imageUrl, display.PNG, 0, 0, false, false))
+    if (!display.drawImage(imageUrl, 0, 0, false, true))
     {
         display.println("Image open error");
         display.display();
@@ -148,7 +212,8 @@ void setup()
     }
 
     Serial.print("Time to sleep: ");
-    Serial.println(timeToSleep);
+    // int timeToSleep = 60;
+    // Serial.println(timeToSleep);
     sendToDeepSleep(timeToSleep * 60);
 }
 
